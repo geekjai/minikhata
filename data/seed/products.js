0122a6107b76d355f5957e73f4fa4ed442fcef13
Jai Kishan <jakishan@oradev.oraclecorp.com>
2021-03-24 18:25:39 +0530

    fixed select option

--- data/seed/products.js
+++ data/seed/products.js
@@ -14,5 +14,41 @@ PRODUCT.SCHEMA.bulkCreate([
 
         categoryId: 1,
         subCategoryId: 1
+    },
+    {
+        productId: 2,
+        productCode: 'AGARBATTI_PREMIX_POWDER',
+        productBarcode: null,
+        productName: 'Agarbatti Premix Powder',
+        description: null,
+        productLink: null,
+        productImage: null,
+
+        categoryId: 1,
+        subCategoryId: 1
+    },
+    {
+        productId: 3,
+        productCode: 'BROWN_JOSH_POWDER',
+        productBarcode: null,
+        productName: 'Brown Josh Powder',
+        description: null,
+        productLink: null,
+        productImage: null,
+
+        categoryId: 1,
+        subCategoryId: 1
+    },
+    {
+        productId: 4,
+        productCode: 'PREMIX_AND_JOSH_POWDER',
+        productBarcode: null,
+        productName: 'Premix and Josh Powder',
+        description: null,
+        productLink: null,
+        productImage: null,
+
+        categoryId: 1,
+        subCategoryId: 1
     }
 ])
